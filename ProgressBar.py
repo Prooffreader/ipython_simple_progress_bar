f46a73f903b98a808b76277cca02594e2539696c
David Taylor <prooffreader@gmail.com>
2015-05-04 07:49:29 -0400

    Update to current version

--- ProgressBar.py
+++ ProgressBar.py
@@ -8,24 +8,25 @@ class ProgressBar:
         self.curr_count = 0
         self.curr_pct = 0
         self.overflow = False
-        print '% complete:',
+        print('% complete: ', end='')
     
     def increment(self):
         self.curr_count += self.increment_size
         if int(self.curr_count) > self.curr_pct:
             self.curr_pct = int(self.curr_count)
             if self.curr_pct <= 100:
-                print self.curr_pct, 
+                print(self.curr_pct, end=' ')
             elif self.overflow == False:
-                print "\n*!* Count has gone over 100%; likely either due to:\n*!*   - an error in the loop_length specified when " + \
-                      "progress_bar was instantiated\n*!*   - an error in the placement of the increment() function"
-                print '*!* Elapsed time when progress bar full: %0.1f seconds.' % (time.time() - self.start)
+                print("\n* Count has gone over 100%; likely either due to:\n  - an error in the loop_length specified when " + \
+                      "progress_bar was instantiated\n  - an error in the placement of the increment() function")
+                print('Elapsed time when progress bar full: {:0.1f} seconds.'.format(time.time() - self.start))
                 self.overflow = True
 
     def finish(self):
-        if self.curr_pct == 99:
-            print "100", # this is a cheat, because rounding sometimes makes the maximum count 99. One day I'll fix this bug.
-        if self.overflow == True:
-            print '*!* Elapsed time after end of loop: %0.1f seconds.\n' % (time.time() - self.start)
+        if 99 <= self.curr_pct <= 100: # rounding sometimes makes the maximum count 99.
+            print("100", end=' ')
+            print('\nElapsed time: {:0.1f} seconds.\n'.format(time.time() - self.start))
+        elif self.overflow == True:
+            print('Elapsed time after end of loop: {:0.1f} seconds.\n'.format(time.time() - self.start))
         else:
-            print '\nElapsed time: %0.1f seconds.\n' % (time.time() - self.start)
+            print('\n* End of loop reached earlier than expected.\nElapsed time: {:0.1f} seconds.\n'.format(time.time() - self.start))
